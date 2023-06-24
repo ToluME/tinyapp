a5d796db31a9b89347f66b32ae22787859612491
Tolu Eshiet <tmeshiet@gmail.com>
2023-06-24 09:05:39 -0600

    Passed username to all views

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -10,6 +11,12 @@ const urlDatabase = {
 };
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use((req, res, next) => {// Middleware to pass username to views
+  res.locals.username = req.cookies.username;
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -21,14 +28,17 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const username = req.cookies.username;
+  res.render("urls_new",{ username: username });
 });
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id],username: req.cookies["username"]  };
   res.render("urls_show", templateVars);
 });
 // Redirect shortURL
@@ -58,8 +68,11 @@ app.post("/urls/:id/delete", (req, res) => { //post request to handle delete and
   delete urlDatabase[id];
   res.redirect("/urls");
 });
+// app.get("/login", (req, res) => {
+//   res.render("login", { username: req.cookies.username })
+// });
 app.post("/login", (req, res) => { //post request to handle login by username
-  const {username} = req.body;
+  const username = req.body.username;
   res.cookie("username", username);
   res.redirect("/urls");
 });
