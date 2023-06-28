198e0d887d8b1409d80a331c27ee9def48763ed8
Tolu Eshiet <tmeshiet@gmail.com>
2023-06-28 17:27:37 -0600

    Created POST registration handler

--- express_server.js
+++ express_server.js
@@ -9,72 +9,97 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use((req, res, next) => {// Middleware to pass username to views
-  res.locals.username = req.cookies.username;
+app.use((req, res, next) => {// Middleware to pass users to views
+  res.locals.user = users[req.cookies.user_id];
   next();
 });
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 app.get("/urls", (req, res) => {
+  const userId = req.cookies.user_id;
+  const user = users[userId];
   const templateVars = {
-    username: req.cookies["username"],
-    urls: urlDatabase };
+    user: user,
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
+
 app.get("/urls/new", (req, res) => {
-  const username = req.cookies.username;
-  res.render("urls_new",{ username: username });
+  const user = req.cookies.users;
+  res.render("urls_new", { user: user });
 });
+
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id],username: req.cookies["username"]  };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    user: req.cookies["users"]
+  };
   res.render("urls_show", templateVars);
 });
-// Redirect shortURL
+
 app.get("/u/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
   res.redirect(longURL);
 });
-// Route handler for handling POST requests to /urls and redirect
+
 app.post("/urls", (req, res) => {
   const longURL = req.body.longURL;
   const id = generateRandomString();
   urlDatabase[id] = longURL;
   res.redirect(`/urls/${id}`);
 });
+
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   const newLongURL = req.body.longURL;
-
-  // Update the value of the stored long URL based on the new value
   urlDatabase[id] = newLongURL;
-
   res.redirect('/urls');
 });
-app.post("/urls/:id/delete", (req, res) => { //post request to handle delete and redirect
+
+app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
   res.redirect("/urls");
 });
-app.post("/login", (req, res) => { //post request to handle login by username
-  const username = req.body.username;
-  res.cookie("username", username);
+
+app.post("/login", (req, res) => {
+  const users = req.body.users;
+  res.cookie("users", users);
   res.redirect("/urls");
 });
-app.post("/logout", (req, res) => {//post to handle logout
-  res.clearCookie("username");
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("users");
   res.redirect("/urls");
 });
 
@@ -82,12 +107,25 @@ app.get('/register', (req, res) => {
   res.render('registration');
 });
 
+app.post("/register", (req, res) => {
+  const { email, password } = req.body;
+  const userId = generateRandomString();
+  const newUser = {
+    id: userId,
+    email: email,
+    password: password,
+  };
+  users[userId] = newUser;
+  res.cookie("user_id", userId);
+  console.log(users);
+  res.redirect("/urls");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-const generateRandomString = function() { //generate random short URL ID
+const generateRandomString = function() {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const length = 6;
   let randomString = '';
