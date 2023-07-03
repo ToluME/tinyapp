3a4bfe97bf3657e2c979c0ce34fb4741b2e1f910
Tolu Eshiet <tmeshiet@gmail.com>
2023-07-02 23:45:37 -0600

    Added more function

--- helpers.js
+++ helpers.js
@@ -1,10 +1,23 @@
-const getUserByEmail = function(email, users) {
-  for (const userId in users) {
-    const user = users[userId];
-    if (user.email === email) {
-      return user;
+module.exports = {
+  getUserByEmail: function(email, users) {
+    for (const userId in users) {
+      const user = users[userId];
+      if (user.email === email) {
+        return user;
+      }
     }
-  }
-  return undefined;
+    return undefined;
+  },
+  generateRandomString: function() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const length = 6;
+    let randomString = '';
+    
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      randomString += characters.charAt(randomIndex);
+    }
+    
+    return randomString;
+  },
 };
-module.exports = getUserByEmail;
